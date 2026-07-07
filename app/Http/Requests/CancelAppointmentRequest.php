0697d390b977aff024994ed99410470edc103fd0
jarvasijudit <jarvasi.judit@gmail.com>
2026-07-07 10:07:01 +0200

    change cancellation_reason column type

--- app/Http/Requests/CancelAppointmentRequest.php
+++ app/Http/Requests/CancelAppointmentRequest.php
@@ -23,7 +23,7 @@ class CancelAppointmentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'cancellation_reason' => ['sometimes', 'string'],
+            'cancellation_reason' => ['sometimes', 'string', 'max:255'],
         ];
     }
 }
